12b650d138beba5741c0edf716f7a9d077346d26
MD SAGOR HOSSAIN <sagor.bme.iu@gmail.com>
2025-11-07 18:59:39 +0600

    Create odconv.py

---
+++ odconv.py
@@ -0,0 +1,10 @@
+# ODConv_pneumonia-xray-images
+from .resnet import ResNet
+from .mobilenetv2 import MobileNetV2
+from .od_resnet import OD_ResNet
+from .od_mobilenetv2 import OD_MobileNetV2
+
+from .odconv import ODConv
+__all__ = [
+    'ResNet', 'MobileNetV2', 'OD_ResNet', 'OD_MobileNetV2'
+]

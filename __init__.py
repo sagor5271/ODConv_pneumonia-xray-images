97af49faa7824fb025f5d6091a21bcdc9c812411
MD SAGOR HOSSAIN <sagor.bme.iu@gmail.com>
2025-11-07 19:32:56 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,3 @@
 # ODConv_pneumonia-xray-images
 from .resnet import ResNet
-from .mobilenetv2 import MobileNetV2
-from .od_resnet import OD_ResNet
-from .od_mobilenetv2 import OD_MobileNetV2
-
 from .odconv import ODConv2d
-__all__ = [
-    'ResNet', 'MobileNetV2', 'OD_ResNet', 'OD_MobileNetV2'
-]
